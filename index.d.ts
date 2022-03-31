fb1fe742584b9a67e761060c1129f8b9055d858f
muslax <muslax@gmail.com>
2022-04-01 05:16:58 +0700

    v0.0.1

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export declare function getSemuaKataBenda(): string[];
+export declare function getKataBenda(length?: number): String[];
+export declare function getSemuaKataSifat(): string[];
+export declare function getKataSifat(length?: number): String[];
